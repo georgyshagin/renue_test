8e20c106b9ac632a101e4206112e36ab958aef34
Georgy Shagin <nas3headlinerov@yandex.ru>
2023-02-01 19:21:16 +0500

    add BuyBlock component

---
+++ src/components/BuyBlock/BuyBlock.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import { classnames } from "classnames";
+import styles from "./styles.module.css";
+
+export const BuyBlock = ({ children, title, subtitle, descr }) => {
+  return (
+    <div className={styles.root}>
+      <h2>{title}</h2>
+      <h3 className={styles.subtitle}>{subtitle}</h3>
+      <h4 className={styles.descr}>{descr}</h4>
+      {children}
+    </div>
+  );
+};
--- src/components/MoneyBlock/MoneyBlock.jsx
+++ src/components/MoneyBlock/MoneyBlock.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 import { Button } from "../Button/Button";
+import { BuyBlock } from "../BuyBlock/BuyBlock";
 
 import styles from "./styles.module.css";
 
 export const MoneyBlock = () => {
   return (
     <div className={styles.root}>
-      <div className={styles.block}>
-        <h2>Внесите деньги</h2>
-        <h4>Принимает: 50, 100, 500, 1000</h4>
-        <div>
-          <input type="number" />
-        </div>
+      <BuyBlock title={"Внесите деньги"}>
+        <form className={styles.form}>
+          <select name="enterMoney" className={styles.enterMoney}>
+            <option value="50">50</option>
+            <option value="100">100</option>
+            <option value="500">500</option>
+            <option value="1000">1000</option>
+          </select>
+        </form>
         <Button>Внести</Button>
-      </div>
-      <div className={styles.block}>
-        <h2>Ваш баланс: {1000}</h2>
-      </div>
-      <div className={styles.block}>
-        <h2>Окно выдачи товара</h2>
-        <h2>Ваш товар: {"milkyway"}</h2>
-      </div>
-      <div className={styles.block}>
-        <h2>Возьмите сдачу</h2>
+      </BuyBlock>
+
+      <BuyBlock title={`Ваш баланс: ${1000}`}></BuyBlock>
+
+      <BuyBlock
+        title={"Окно выдачи товара"}
+        subtitle={`Ваш товар: ${"milkyway"}`}
+      ></BuyBlock>
+
+      <BuyBlock title={"Возьмите сдачу"} subtitle={`Ваша сдача: ${273}`}>
         <Button>Получить сдачу</Button>
-        <h3>Ваша сдача: {23}</h3>
-      </div>
+      </BuyBlock>
     </div>
   );
 };
